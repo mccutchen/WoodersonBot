7d6670823f9dad4637e4989c62454167ca668009
Will McCutchen <wm@bitly.com>
2012-01-22 13:01:37 -0500

    Add static files

--- main.py
+++ main.py
@@ -2,21 +2,12 @@ import webapp2
 import wooderson
 
 
-class IndexHandler(webapp2.RequestHandler):
-  def get(self):
-      self.response.headers['Content-Type'] = 'text/plain'
-      self.response.out.write('Alright alright alright')
-
-
 class SocializeHandler(webapp2.RequestHandler):
     def get(self):
-        replies = wooderson.socialize()
+        wooderson.socialize()
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.out.write('OK')
 
 
-public_urls = [('/', IndexHandler)]
-private_urls = [('/cron/socialize', SocializeHandler)]
-
-public_app = webapp2.WSGIApplication(public_urls, debug=True)
-private_app = webapp2.WSGIApplication(private_urls, debug=True)
+urls = [('/cron/socialize', SocializeHandler)]
+app = webapp2.WSGIApplication(urls, debug=True)
