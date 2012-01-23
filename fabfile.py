0eff2db4e1702fecd9f9b606d8b4bf46bb53db24
Will McCutchen <wm@bitly.com>
2012-01-23 18:58:55 -0500

    Use gaefab as submodule

--- fabfile.py
+++ fabfile.py
@@ -1,1 +1,11 @@
-from lib.fabric import *
+from ext.gaefab import *
+
+def pre_deploy_hook(tag, export):
+    """Ensure that the secrets.py file is copied in from a safe place before
+    deploying.
+    """
+    if not os.path.exists('secrets.py'):
+        local('scp overloaded.org:woodersonbot/secrets.py .')
+    return True
+
+deploy.pre_deploy_hook = pre_deploy_hook
